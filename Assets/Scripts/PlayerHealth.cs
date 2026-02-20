cd86fa0abed9a250bd234e4093213d3207c2e069
Bruno <jordanyytb4@gmail.com>
2026-02-20 17:41:24 +0100

    Add invulnerability feature to player health system

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,5 +1,7 @@
 using UnityEngine;
 using TMPro;
+using NUnit.Framework;
+using System.Collections;
 
 public class PlayerHealth : MonoBehaviour
 {
@@ -12,6 +14,9 @@ public class PlayerHealth : MonoBehaviour
     [SerializeField]
     private TextMeshProUGUI currentLifePointsText;
 
+    [SerializeField]
+    private bool inVulnerable = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
@@ -22,18 +27,30 @@ public class PlayerHealth : MonoBehaviour
 
     // Update is called once per frame
     public void TakeDamage(int damage = 1)
-
     {
+        if (inVulnerable)
         {
-            // Mathf.Clamp permet de limiter la valeur entre deux bornes
-            currentlifePoints = Mathf.Clamp(currentlifePoints - damage, 0, maxLifePoints);
+            return;
+        }
+        inVulnerable = true;
+        StartCoroutine(InvulnerableDuration());
 
-            if (currentlifePoints == 0)
-            {
-                Debug.Log("Game over");
-            }
+        // Mathf.Clamp permet de limiter la valeur entre deux bornes
+        currentlifePoints = Mathf.Clamp(currentlifePoints - damage, 0, maxLifePoints);
+        currentLifePointsText.SetText(currentlifePoints.ToString());
+
+        if (currentlifePoints == 0)
+        {
+            Debug.Log("Game over");
         }
     }
+
+    IEnumerator InvulnerableDuration()
+    {
+        float duration = 5f;
+
+        yield return new WaitForSeconds(duration);
+
+        inVulnerable = false;
+    }
 }
-
-
